0e71173d28c9e5122c1e2123e67d998b6d74d75a
苏乞儿 <41564304+suqir@users.noreply.github.com>
2022-11-07 21:26:10 +0800

    修改svip规则

--- Surge/JS/CamScanner.js
+++ Surge/JS/CamScanner.js
@@ -30,5 +30,5 @@ hostname = ap*.intsig.net
 
 **************************/
 let obj = JSON.parse($response.body);
-obj = {"data":{"psnl_vip_property":{"expiry":"1687017600"}}};
+obj = {"data":{"psnl_vip_property":{"expiry":"1687017600","svip": 1}}};
 $done({body: JSON.stringify(obj)});
